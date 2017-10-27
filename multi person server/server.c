e55890e818ed3dc5bb1ae686d42d237450c14a77
Nazanin Sabri <nazanin.sabrii@gmail.com>
2017-10-27 19:49:04 +0330

    add multi person server

---
+++ multi person server/server.c
@@ -0,0 +1,296 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+//This function gets a char digit and returns the int value 
+//if the char is not a digit it returns -1
+int charDigitToInt(char c){
+	int ret; 
+	switch(c){
+		case '0':
+			ret = 0; 
+			break;
+		case '1':
+			ret = 1; 
+			break;
+		case '2':
+			ret = 2; 
+			break;
+		case '3':
+			ret = 3; 
+			break;
+		case '4':
+			ret = 4; 
+			break;
+		case '5':
+			ret = 5; 
+			break;
+		case '6':
+			ret = 6; 
+			break;
+		case '7':
+			ret = 7; 
+			break;
+		case '8':
+			ret = 8; 
+			break;
+		case '9':
+			ret = 9; 
+			break;
+		default:
+			ret = -1;
+			break;
+
+	}
+	return ret;
+}
+//This function gets a char array and returns the int equivalent
+int convertCharPortToInt(char *buff, int size, int numOfRead){
+	int converted = 0;
+	int pow = 1;
+	int base = 10;
+	for(int position = numOfRead -1; position>=0; position--){
+		int temp = charDigitToInt(buff[position]);
+		if(temp==-1){
+			write(1, "Invalid Port Number\n", 20);
+			_exit(1);
+		}
+		converted += temp*pow;
+		pow *= base;
+	}
+	return converted;
+}
+//This function reads the port number using read()
+//then passes it to another function to be converted into int
+int readNumber(){
+	char buff[10];
+	char numbuff[1024];
+	int place = 0;
+	int n;
+	write(1, "Port: ", 6);
+	while( (n = read(0, buff, sizeof(buff)>0)) !=0){
+		if(n==1 && (buff[0]=='\n'))
+			break;
+		numbuff[place] = buff[0];
+		place++;
+	}
+	numbuff[place] = '\0';
+	return convertCharPortToInt(numbuff, 1024, place);
+}
+//This function teads the ip using read()
+void readString(char* ip, int size){
+	char buff[10];
+	char ipbuff[16];
+	int place = 0;
+	int n;
+	write(1, "IP: ", 3);
+	while( (n = read(0, buff, sizeof(buff)>0)) !=0){
+		if(n==1 && (buff[0]=='\n'))
+			break;
+		ipbuff[place] = buff[0];
+		place++;
+	}
+	ipbuff[place] = '\0';
+	strcpy(ip, ipbuff);
+}
+void *get_in_addr(struct sockaddr *sa){
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+//This is a socket that only sends one message
+void old_socket(){
+	//variable decelerations
+  	int welcomeSocket, newSocket;
+  	char buffer[1024];
+  	struct sockaddr_in serverAddr;
+  	struct sockaddr_storage serverStorage;
+  socklen_t addr_size;
+
+  /*---- Create the socket. The three arguments are: ----*/
+  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+  
+
+  //reading the port number from stdin
+	int port_number = readNumber();
+	//I considered the maximum length of the ip to be \0xxx.yyy.zzz.ttt
+	//making it 15 bits
+	char ip[16];
+	//reading the ip from the stdin
+	readString(ip, sizeof(ip));
+
+
+  /*---- Configure settings of the server address struct ----*/
+  /* Address family = Internet */
+  serverAddr.sin_family = AF_INET;
+  /* Set port number, using htons function to use proper byte order */
+  serverAddr.sin_port = htons(port_number);
+  /* Set IP address to localhost */
+  serverAddr.sin_addr.s_addr = inet_addr(ip);
+  /* Set all bits of the padding field to 0 */
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  /*---- Bind the address struct to the socket ----*/
+  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  /*---- Listen on the socket, with 5 max connection requests queued ----*/
+  if(listen(welcomeSocket,5)==0)
+    printf("Listening\n");
+  else
+    printf("Error\n");
+
+  /*---- Accept call creates a new socket for the incoming connection ----*/
+  addr_size = sizeof serverStorage;
+  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+
+  /*---- Send message to the socket of the incoming connection ----*/
+  strcpy(buffer,"Hello World\n");
+  send(newSocket,buffer,13,0);
+}
+void workWithSocket(){
+	fd_set master;    // master file descriptor list
+    fd_set read_fds;  // temp file descriptor list for select()
+    int fdmax;        // maximum file descriptor number
+
+    int listener;     // listening socket descriptor
+    int newfd;        // newly accept()ed socket descriptor
+    struct sockaddr_storage remoteaddr; // client address
+    socklen_t addrlen;
+
+    char buf[256];    // buffer for client data
+    int nbytes;
+
+	char remoteIP[INET6_ADDRSTRLEN];
+
+    int yes=1;        // for setsockopt() SO_REUSEADDR, below
+    int i, j, rv;
+
+	struct addrinfo hints, *ai, *p;
+
+    FD_ZERO(&master);    // clear the master and temp sets
+    FD_ZERO(&read_fds);
+
+	// get us a socket and bind it
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
+		fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+		exit(1);
+	}
+	
+	for(p = ai; p != NULL; p = p->ai_next) {
+    	listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (listener < 0) { 
+			continue;
+		}
+		
+		// lose the pesky "address already in use" error message
+		setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+		if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+			close(listener);
+			continue;
+		}
+
+		break;
+	}
+
+	// if we got here, it means we didn't get bound
+	if (p == NULL) {
+		fprintf(stderr, "selectserver: failed to bind\n");
+		exit(2);
+	}
+
+	freeaddrinfo(ai); // all done with this
+
+    // listen
+    if (listen(listener, 10) == -1) {
+        perror("listen");
+        exit(3);
+    }
+
+    // add the listener to the master set
+    FD_SET(listener, &master);
+
+    // keep track of the biggest file descriptor
+    fdmax = listener; // so far, it's this one
+
+    // main loop
+    for(;;) {
+        read_fds = master; // copy it
+        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(4);
+        }
+
+        // run through the existing connections looking for data to read
+        for(i = 0; i <= fdmax; i++) {
+            if (FD_ISSET(i, &read_fds)) { // we got one!!
+                if (i == listener) {
+                    // handle new connections
+                    addrlen = sizeof remoteaddr;
+					newfd = accept(listener,
+						(struct sockaddr *)&remoteaddr,
+						&addrlen);
+
+					if (newfd == -1) {
+                        perror("accept");
+                    } else {
+                        FD_SET(newfd, &master); // add to master set
+                        if (newfd > fdmax) {    // keep track of the max
+                            fdmax = newfd;
+                        }
+                        printf("selectserver: new connection from %s on "
+                            "socket %d\n",
+							inet_ntop(remoteaddr.ss_family,
+								get_in_addr((struct sockaddr*)&remoteaddr),
+								remoteIP, INET6_ADDRSTRLEN),
+							newfd);
+                    }
+                } else {
+                    // handle data from a client
+                    if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+                        // got error or connection closed by client
+                        if (nbytes == 0) {
+                            // connection closed
+                            printf("selectserver: socket %d hung up\n", i);
+                        } else {
+                            perror("recv");
+                        }
+                        close(i); // bye!
+                        FD_CLR(i, &master); // remove from master set
+                    } else {
+                        // we got some data from a client
+                        for(j = 0; j <= fdmax; j++) {
+                            // send to everyone!
+                            if (FD_ISSET(j, &master)) {
+                                // except the listener and ourselves
+                                if (j != listener && j != i) {
+                                    if (send(j, buf, nbytes, 0) == -1) {
+                                        perror("send");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } // END handle data from client
+            } // END got new incoming connection
+        } // END looping through file descriptors
+    } // END for(;;)--and you thought it would never end!
+}
+int main(){
+	//old_socket();
+	workWithSocket();
+}
