8efaa53e56f45b4cb49096b6421c181f1b54c581
Nazanin Sabri <nazanin.sabrii@gmail.com>
2017-10-30 10:14:14 +0330

    no need to disconnect the client and the port was read reverse before it is corrected in this version

---
+++ auto open other server/miniServer.c
@@ -0,0 +1,625 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define BUFSIZE 1024
+#define FILEBUFF 10000
+//the stuct to keep file part info
+typedef struct File{
+	char fileName[100];
+	char ip[16];
+	int port;
+	int part;
+	struct File* nextFile;
+}File;
+typedef struct ThisSystem{
+	char ip[16]; 
+	int port;
+}ThisSystem;
+char intToCharDigit(int c){
+	char ret; 
+	switch(c){
+		case 0:
+			ret = '0'; 
+			break;
+		case 1:
+			ret = '1'; 
+			break;
+		case 2:
+			ret = '2'; 
+			break;
+		case 3:
+			ret = '3'; 
+			break;
+		case 4:
+			ret = '4'; 
+			break;
+		case 5:
+			ret = '5'; 
+			break;
+		case 6:
+			ret = '6'; 
+			break;
+		case 7:
+			ret = '7'; 
+			break;
+		case 8:
+			ret = '8'; 
+			break;
+		case 9:
+			ret = '9'; 
+			break;
+		default:
+			ret = '-';
+			break;
+
+	}
+	return ret;
+}
+void reverse_string(char* str){
+	char temp;
+	int i = 0;
+    int j = strlen(str) - 1;
+ 
+   	while (i < j) {
+      	temp = str[i];
+      	str[i] = str[j];
+      	str[j] = temp;
+      	i++;
+      	j--;
+    }
+}
+void intToCharString(int x, char* charEq){
+	int a; 
+	int counter = 0; 
+	while(x>0){
+		a = x%10; 
+		charEq[counter] = intToCharDigit(a);
+		if(charEq[counter]=='-'){
+			write(1, "Invalid Port Number\n", 20);
+			_exit(1);
+		}
+		x = x - x%10; 
+		x = x/10;
+		counter +=1;
+	}
+	reverse_string(charEq);
+}
+void print_node_info(File** curr){
+	char port[10]; 
+	char part[10]; 
+	intToCharString((*curr)->part, part);
+	intToCharString((*curr)->port, port);
+	write(1, "File Name: ", 11); 
+	write(1, (*curr)->fileName, sizeof((*curr)->fileName));
+	write(1, " Part: ", 7);
+	write(1, part, sizeof((*curr)->part));
+	write(1, " available at port: ", 20);
+	write(1, port, sizeof((*curr)->port)); 
+	write(1, " ip: ", 5);
+	write(1, (*curr)->ip, sizeof((*curr)->ip));
+	write(1, "\n", 1);
+}
+void make_file_info_message(File** curr, char* buff){
+	char port[10]; 
+	char part[10]; 
+	memset(part, 0, sizeof(part));
+	memset(port, 0, sizeof(port));
+	intToCharString((*curr)->part, part);
+	intToCharString((*curr)->port, port);
+	strcpy(buff, "*File Name: ");
+	strcat(buff, (*curr)->fileName);
+	strcat(buff, " Part: ");
+	strcat(buff, part);
+	strcat(buff, " available at port: ");
+	strcat(buff, port);
+	strcat(buff, " and IP: ");
+	strcat(buff, (*curr)->ip);
+	strcat(buff, "\n");
+}
+//This function gets a char digit and returns the int value 
+//if the char is not a digit it returns -1
+int charDigitToInt(char c){
+	int ret; 
+	switch(c){
+		case '0':
+			ret = 0; 
+			break;
+		case '1':
+			ret = 1; 
+			break;
+		case '2':
+			ret = 2; 
+			break;
+		case '3':
+			ret = 3; 
+			break;
+		case '4':
+			ret = 4; 
+			break;
+		case '5':
+			ret = 5; 
+			break;
+		case '6':
+			ret = 6; 
+			break;
+		case '7':
+			ret = 7; 
+			break;
+		case '8':
+			ret = 8; 
+			break;
+		case '9':
+			ret = 9; 
+			break;
+		default:
+			ret = -1;
+			break;
+
+	}
+	return ret;
+}
+int convertCharArrayToInt(char* arr, int size){
+	int converted = 0;
+	int pow = 1;
+	int base = 10;
+	for(int position = 0; position<size; position++){
+		int temp = charDigitToInt(arr[position]);
+		if(temp==-1){
+			write(1, "Invalid Port Number\n", 20);
+			_exit(1);
+		}
+		converted += temp*pow;
+		pow *= base;
+	}
+	return converted;
+}
+//This function gets a char array and returns the int equivalent
+int convertCharPortToInt(char *buff, int size, int numOfRead){
+	int converted = 0;
+	int pow = 1;
+	int base = 10;
+	for(int position = numOfRead -1; position>=0; position--){
+		int temp = charDigitToInt(buff[position]);
+		if(temp==-1){
+			write(1, "Invalid Port Number\n", 20);
+			_exit(1);
+		}
+		converted += temp*pow;
+		pow *= base;
+	}
+	return converted;
+}
+//This function reads the port number using read()
+//then passes it to another function to be converted into int
+int readNumber(){
+	char buff[10];
+	char numbuff[1024];
+	int place = 0;
+	int n;
+	memset(buff, 0, sizeof(buff));
+	memset(numbuff, 0, sizeof(numbuff));
+	while( (n = read(0, buff, sizeof(buff)>0)) !=0){
+		if(n==1 && (buff[0]=='\n'))
+			break;
+		numbuff[place] = buff[0];
+		place++;
+	}
+	numbuff[place] = '\0';
+	return convertCharPortToInt(numbuff, 1024, place);
+}
+//This function teads the ip using read()
+void readString(char* ip, int size){
+	char buff[10];
+	char ipbuff[16];
+	int place = 0;
+	int n;
+	while( (n = read(0, buff, sizeof(buff)>0)) !=0){
+		if(n==1 && (buff[0]=='\n'))
+			break;
+		ipbuff[place] = buff[0];
+		place++;
+	}
+	ipbuff[place] = '\0';
+	strcpy(ip, ipbuff);
+}
+void *get_in_addr(struct sockaddr *sa){
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+File* search_for_file(File** head, char* name, int size, int part){
+	File* curr = (*head);
+	while(curr!=NULL){
+		if(strcmp(curr->fileName,name)==0){
+			if(curr->part==part)
+				return curr;
+		}
+		curr = curr->nextFile;
+	}
+	return NULL;
+}
+void add_file_part(File** head, int _port, int _part, char* name, int size, char* ip, int sizeIP){
+	File* newfile; 
+	File* curr;
+	curr = (struct File*)malloc(sizeof(struct File));
+	newfile = (struct File*)malloc(sizeof(struct File));
+	curr= (*head);
+	//fill the data for the new node
+	memset(newfile->fileName, 0, sizeof(newfile->fileName));
+	memset(newfile->ip, 0, sizeof(newfile->ip));
+	strcpy(newfile->fileName, name);
+	strcpy(newfile->ip, ip);
+	newfile->port = _port; 
+	newfile->part = _part;
+	newfile->nextFile = NULL;
+	while(curr!=NULL){
+		if(curr->nextFile!=NULL)
+			curr = curr->nextFile;
+		else 
+			break;
+	}
+	curr->nextFile = newfile;
+}
+/*
+	the whole thing is 256 chars and we will order it like this:
+	<garbage>+<file part: 5>+<file name:100>+<port number: 5>+<ip:16>i
+	i stands for info
+*/
+void encode_resources(File** curr, char* buff){	
+	char temp[256];
+	char port[5];
+	char part[5];
+	memset(temp, 0, sizeof(temp));
+	intToCharString((*curr)->port, port);
+	intToCharString((*curr)->part, part);
+	strcpy(temp, "i");
+	strcat(temp, (*curr)->ip);
+	strcat(temp, "+");
+	strcat(temp, port);
+	strcat(temp, "+");
+	strcat(temp, (*curr)->fileName);
+	strcat(temp, "+");
+	strcat(temp, part);
+	strcat(temp, "+");
+	strcat(temp, "\0");
+
+	strcpy(buff, temp);
+}
+int iSaDigitOrDot(char c){
+	if(c=='1' || c=='2' || c=='3' || c=='4' || c=='5' || c=='6' || c=='7' || c=='8' || c=='9' || c=='0' || c=='.' || c==' ')
+		return 1;
+	return 0;
+}
+void decode_Resource_from_socket(File** head, char* resourceString, int size){
+	int _part, _port; 
+	char ip[16];
+	char file[100];
+	char partTemp[5];
+	char portTemp[5];
+	int stringcounter = 1;
+	int ipcounter = 0;
+	int pluscount=0;
+	int portcounter =0;
+	int filecounter=0;
+	int partcounter =0;
+	memset(ip, 0, sizeof(ip));
+	memset(file, 0, sizeof(file));
+	for(int i=1; i<size; i++){
+		if(resourceString[i]=='+'){
+			pluscount+=1;
+			continue;
+		}
+		if(pluscount==5)
+			break;
+		if(pluscount==0){
+			ip[ipcounter]=resourceString[i];
+			ipcounter+=1;
+			continue;
+		}
+		if(pluscount==1){
+			portTemp[portcounter]=resourceString[i];
+			portcounter+=1;
+			continue;
+		}
+		if(pluscount==2){
+			file[filecounter]=resourceString[i];
+			filecounter+=1;
+			continue;
+		}
+		if(pluscount==3){
+			partTemp[partcounter]=resourceString[i];
+			partcounter+=1;
+			continue;
+		}
+	}
+	for (int i = 0; i < sizeof(ip); i++){
+		if(!iSaDigitOrDot(ip[i]))
+			ip[i]='\0';
+	}
+	for (int i = 0; i < sizeof(partTemp); i++){
+		if(!iSaDigitOrDot(partTemp[i]))
+			partTemp[i]='\0';
+	}
+	for (int i = 0; i < sizeof(portTemp); i++){
+		if(!iSaDigitOrDot(portTemp[i]))
+			portTemp[i]='\0';
+	}
+	_port = convertCharArrayToInt(portTemp, portcounter);
+	_part = convertCharArrayToInt(partTemp, partcounter);
+	add_file_part(head, _port, _part, file, sizeof(file), ip, sizeof(ip));
+}
+void get_get_part_message(char* file, char* part){
+	write(1, "The name of the file you want: ", 31);
+	readString(file, sizeof(file));
+	write(1, "Which part? ", 12);
+	readString(part, sizeof(part));
+}
+/*
+	we encode it like this
+		g(which stands for get)<file name>+<part>+
+*/
+void encode_get_part_message(char* msg){
+	char file[100];
+	char part[5];
+	memset(file, 0, sizeof(file));
+	memset(part, 0, sizeof(part));
+	get_get_part_message(file, part);
+	strcpy(msg, "g");
+	strcat(msg, file);
+	strcat(msg, "+");
+	strcat(msg, part);
+	strcat(msg,"+");
+}
+int decode_get_part_message(char* msg, char* fileName){
+	char file[100];
+	char part[5];
+	memset(file, 0, sizeof(file));
+	memset(part, 0, sizeof(part));
+	int filecounter = 0;
+	int partcounter = 0;
+	int pluscount=0;
+	for(int i=1; i<sizeof(msg); i++){
+		if(msg[i]=='+'){
+			pluscount+=1;
+			continue;
+		}
+		if(pluscount==2)
+			break;
+		if(pluscount==0){
+			file[filecounter]=msg[i];
+			filecounter+=1;
+			continue;
+		}
+		if(pluscount==1){
+			part[partcounter]=msg[i];
+			partcounter+=1;
+			continue;
+		}
+	}
+	strcpy(fileName, file);
+	int partN = convertCharArrayToInt(part, partcounter);
+	return partN;
+}
+void get_available_resources(char* ip, int size, int port_number, File** head){
+	char name[100]; 
+	int part_number;
+	write(1, "Enter 0 when you are done.\n", 27);
+	do{
+		memset(name, 0, sizeof(name));
+		write(1, "File Name: ", 11);
+		readString(name, sizeof(name));
+		if(name[0]=='0')
+			break;
+		write(1, "Part Number: ", 13); 
+		part_number = readNumber();
+		if(part_number==0)
+			break;
+		add_file_part(head, port_number, part_number, name, sizeof(name), ip, sizeof(ip));
+	}while(name[0]!='0' && part_number!=0);
+}
+void print_available_resources(File** head){
+	File* curr = (*head); 
+	if(curr==NULL){
+		write(1, "No available resources\n", 23);
+		return;
+	}
+	curr= curr->nextFile;
+	while(curr!=NULL){
+		print_node_info(&curr); 
+		curr =  curr->nextFile;
+	}
+}
+void set_up_linkedlist_head(File** head){
+	(*head) = (struct File*)malloc(sizeof(struct File));
+	(*head)->port = 0; 
+	(*head)->part = 0;
+	strcpy((*head)->fileName, "0");
+	strcpy((*head)->ip, "0");
+	(*head)->nextFile = NULL;
+}
+void read_and_write_file_content(char* fileName){
+	char buffer [BUFSIZE]; 
+  	int length; 
+  	int fd; 
+   
+  	/* Open the file, print error message if we fail */
+  	if ( ( fd = open (fileName, O_RDONLY) ) < 0 ) { 
+    	write(1, "Error\n", 6);
+    	return;
+  	} 
+
+  	/* Copy file contents to stdout, stop when read returns 0 (EOF) */
+  	while ( (length = read (fd, buffer, BUFSIZE)) > 0 ) 
+     	write (1, buffer, length); 
+
+   	write(1, "\n", 1);
+  	close (fd); 
+}
+void read_file_content(char* fileName, char* buff){
+	char buffer [BUFSIZE]; 
+  	int length; 
+  	int fd; 
+   
+  	/* Open the file, print error message if we fail */
+  	if ( ( fd = open (fileName, O_RDONLY) ) < 0 ) { 
+    	write(1, "Error\n", 6);
+    	return;
+  	} 
+  	strcpy(buff, "");
+  	/* Copy file contents to stdout, stop when read returns 0 (EOF) */
+  	while ( (length = read (fd, buffer, BUFSIZE)) > 0 ) 
+  		strcat(buff, buffer);
+}
+int compare_ip_strings(char* ip1, char*ip2){
+	if(sizeof(ip1)!=sizeof(ip2))
+		return 0;
+	int x=0; 
+	while(x<sizeof(ip1)){
+		if(ip1[x]==ip2[x])
+			x+=1;
+		else
+			return 0;
+	}
+	return 1;
+}
+void handle_get_file(char* msg, File** head, ThisSystem** ourSystem, char* buff){
+	char fileName[100];
+	int part = decode_get_part_message(msg, fileName);
+	File* curr; 
+	curr = search_for_file(head, fileName, sizeof(fileName), part);
+	if(curr==NULL)
+		strcpy(buff, "*No such file in our database\n");
+	else{
+		if( (curr->port==(*ourSystem)->port) && (compare_ip_strings(curr->ip, (*ourSystem)->ip)) ){
+			read_file_content(fileName, buff);
+		}
+		else{
+			make_file_info_message(&curr, buff);
+		}
+	}
+}
+//This is a socket that only sends one message
+void becomeAServer(File** head, ThisSystem** ourSystem){
+	//variable decelerations
+  	int welcomeSocket, newSocket;
+  	char buffer[1024];
+  	char file[100];
+  	char outputbuff[FILEBUFF];
+  	struct sockaddr_in serverAddr;
+  	struct sockaddr_storage serverStorage;
+  	socklen_t addr_size;
+
+  /*---- Create the socket. The three arguments are: ----*/
+  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+
+  /*---- Configure settings of the server address struct ----*/
+  /* Address family = Internet */
+  serverAddr.sin_family = AF_INET;
+  /* Set port number, using htons function to use proper byte order */
+  serverAddr.sin_port = htons((*ourSystem)->port);
+  /* Set IP address to localhost */
+  serverAddr.sin_addr.s_addr = inet_addr((*ourSystem)->ip);
+  /* Set all bits of the padding field to 0 */
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+  /*---- Bind the address struct to the socket ----*/
+  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  while(1){
+  		/*---- Listen on the socket, with 5 max connection requests queued ----*/
+  		if(listen(welcomeSocket,5)==0)
+   	 		write(1, "Listening\n", 10);
+  		else{
+    		write(1, "Error with welcomeSocket\n", 25);
+    		_exit(1);
+  		}
+  		/*---- Accept call creates a new socket for the incoming connection ----*/
+  		addr_size = sizeof serverStorage;
+  		newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+      	memset(file, 0, sizeof(file));
+      	memset(buffer, 0, sizeof(buffer));
+        memset(outputbuff, 0, sizeof(outputbuff));
+      	recv(newSocket, buffer, 1024, 0);
+      	if(buffer[0]=='i'){
+            decode_Resource_from_socket(head, buffer, sizeof(buffer));
+        }
+        //we got file request
+        if(buffer[0]=='g'){
+
+            handle_get_file(buffer, head, ourSystem, outputbuff);
+            send(newSocket, outputbuff, sizeof(outputbuff), 0);
+            memset(buffer, 0, sizeof(buffer));
+        	memset(outputbuff, 0, sizeof(outputbuff));
+        	buffer[0]='\0';
+        	outputbuff[0]='\0';
+        }
+  }
+}
+void BeAclient(File** head){
+  	int clientSocket;
+  	char buffer[BUFSIZE];
+  	struct sockaddr_in serverAddr;
+  	socklen_t addr_size;
+  	char ip[16];
+  	memset(ip, 0, sizeof(ip));
+  	write(1, "Port of the Server you want to connect to: ", 43);
+  	int port = readNumber();
+  	write(1, "IP: ", 4);
+  	readString(ip, sizeof(ip));
+  	/*---- Create the socket. The three arguments are: ----*/
+  	/* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+  	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+  	/*---- Configure settings of the server address struct ----*/
+  	/* Address family = Internet */
+  	serverAddr.sin_family = AF_INET;
+  	serverAddr.sin_port = htons(port);
+  	serverAddr.sin_addr.s_addr = inet_addr(ip);
+  	/* Set all bits of the padding field to 0 */
+  	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  	/*---- Connect the socket to the server using the address struct ----*/
+  	addr_size = sizeof serverAddr;
+  
+  	connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+  	File* curr = (*head)->nextFile;
+  	while(curr!=NULL){
+  		memset(buffer, 0, sizeof(buffer));
+  		encode_resources(&curr, buffer);
+  		send(clientSocket, buffer, sizeof(buffer), 0);
+  		curr = curr->nextFile;
+  	}
+  	close(clientSocket);
+}
+int main(){
+	ThisSystem* ourSystem = (ThisSystem*)malloc(sizeof(ThisSystem));
+	write(1, "This Servers Port: ", 19);
+	int port_number = readNumber();
+	//I considered the maximum length of the ip to be \0xxx.yyy.zzz.ttt making it 16 bits
+	char ip[16];
+	memset(ip, 0, sizeof(ip));
+	memset(ourSystem->ip, 0, sizeof(ip));
+	//reading the ip from the stdin
+	write(1, "This Servers IP: ", 17);
+	readString(ip, sizeof(ip));
+
+	strcpy(ourSystem->ip, ip); 
+	ourSystem->port = port_number;
+
+	File* head; 
+	set_up_linkedlist_head(&head);
+	get_available_resources(ip, sizeof(ip), port_number, &head);
+	print_available_resources(&head);
+
+	BeAclient(&head);
+
+	becomeAServer(&head, &ourSystem);
+}
